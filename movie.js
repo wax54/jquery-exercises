78c4c33495e187ff1287f9a6a94b4594945cf999
sam <wax654@gmail.com>
2021-01-14 22:11:16 -0800

    turned the list into a table so it is easier to search

--- movie.js
+++ movie.js
@@ -1,26 +1,56 @@
 $('#user-input').on('submit', function (e) {
-    console.log('this', this);
     e.preventDefault();
     handleSubmit();
 })
-$('ul').on('click', 'li .delete', function () {
+$('#movie-table').on('click', '.delete', function () {
     $(this).parent().remove();
 });
 
 function handleSubmit() {
     const title = $('#title').val();
-    const rating = $('#rating').val();
-    addToList(`${title} ${rating}`);
+    const rating = +$('#rating').val();
+
+    if (title.length < 2) {
+        inputError('Title must be at least 2 characters long!');
+        return;
+    }
+    if (rating > 10 || rating < 0) {
+        inputError('Rating must be between 0 and 10!');
+        return
+    }
+
+    const listItems = {
+        title,
+        rating
+    }
+    addAsRow(listItems);
 
 }
 
-function addToList(str) {
+function addAsRow(rowItems) {
+    const tr = $('<tr>');
+    for ([key, data] of Object.entries(rowItems)) {
+        //append each key as the td's class, 
+        //  and the element as the text of te td
+        const td = $('<td>')
+        td.addClass(key);
+        td.text(data)
+        tr.append(td);
+    }
+    //append a delete button to the end of the row
+    $('<button>').addClass('delete').text('X').appendTo(tr);
+    $('#movie-table').append(tr);
+}
+function addToRow(tr, td) {
     const delButton = $('<button class="delete">')
         .text('X');
 
-    const li = $(`<li>`)
+    const li = $(`<>`)
         .text(str + ' ')
         .append(delButton);
 
     $('ul').append(li);
+}
+function inputError(msg) {
+    alert(msg);
 }
